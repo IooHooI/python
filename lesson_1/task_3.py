55c87bf1393c0a470ad89afadfbadcd48757605c
Головин Ефим <feanoref@gmail.com>
2021-09-15 13:19:13 +0300

    Task 3 fix

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -5,13 +5,11 @@ def task():
     # 3. Узнайте у пользователя число n.
     # Найдите сумму чисел n + nn + nnn.
     # Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-    print("Введите число n (0 <= n < 10):")
+    print("Введите положительное число n:")
     n = input()
-    n = check(n)
 
-    if n is not None and 0 <= n < 10:
-        m = 3
-        result = sum([sum([n * 10**j for j in range(i + 1)]) for i in range(m)])
+    if check(n) is not None:
+        result = int(n) + int(n + n) + int(n + n + n)
 
         print("Результат: {}".format(result))
     else:
