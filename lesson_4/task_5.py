9d6cbebca24e95fdc0417e167aaeba9470255ce0
Головин Ефим <feanoref@gmail.com>
2021-09-21 15:52:09 +0300

    Task 5

--- lesson_4/task_5.py
+++ lesson_4/task_5.py
@@ -1,8 +1,14 @@
+from functools import reduce
 
 
-def task():
-    pass
+def foo(_min_, _max_):
+    for item in range(_min_, _max_ + 2, 2):
+        yield item
+
+
+def task(_min_, _max_):
+    return reduce(lambda x, y: x * y, [item for item in foo(_min_, _max_)])
 
 
 if __name__ == "__main__":
-    task()
+    print("Результат: {}".format(task(100, 1000)))
